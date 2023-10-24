50f4d6ac94c135e9f396b92a7f2e04007813e2eb
braydenhand <bhand9892@gmail.com>
2023-10-24 15:50:19 +0200

    second

--- microblog/main.py
+++ microblog/main.py
@@ -11,49 +11,52 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    user = model.User(1, "mary@example.com", "mary")
+    user = model.User(email="mary@example.com", name="mary")
     posts = [
-        model.Message(
-            1, user, "Test post", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            2, user, "Another post", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            3, user, "Another post test", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
+            model.Message(
+        user=user,
+        text="Test post 1",
+        timestamp=datetime.datetime.now(dateutil.tz.tzlocal()),
+    ),
+    model.Message(
+        user=user,
+        text="Another post",
+        timestamp=datetime.datetime.now(dateutil.tz.tzlocal()),
+    ),
     ]
     return render_template("main/index.html", posts=posts)
 @bp.route('/profile')
 def profile():
-    user = model.User(1, "tom@example.com", "tom")
+    user = model.User(email="tom@example.com", name="tom")
     posts = [
         model.Message(
-            1, user, "Test post", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            2, user, "Another post", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            3, user, "Another post test", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
+        user=user,
+        text="Test post 1",
+        timestamp=datetime.datetime.now(dateutil.tz.tzlocal()),
+    ),
+    model.Message(
+        user=user,
+        text="Another post",
+        timestamp=datetime.datetime.now(dateutil.tz.tzlocal()),
+    ),
     ]
     return render_template('main/profile.html', posts=posts)
 @bp.route('/posts')
 def posts():
-    user = model.User(1, "jason@example.com", "jason")
+    user = model.User(email="jason@example.com", name="jason")
     post = model.Message(
-            1, user, "Main Post", datetime.datetime.now(dateutil.tz.tzlocal())
+            user=user, text="Main Post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
         )
     replies = [
         model.Message(
-            1, user, "reply post", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            2, user, "reply post2", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            3, user, "Another post test", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
+        user=user,
+        text="Test post 1",
+        timestamp=datetime.datetime.now(dateutil.tz.tzlocal()),
+    ),
+    model.Message(
+        user=user,
+        text="Another post",
+        timestamp=datetime.datetime.now(dateutil.tz.tzlocal()),
+    ),
     ]
     return render_template('main/posts.html', posts=replies, post=post)
